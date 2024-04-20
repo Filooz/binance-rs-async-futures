72c3a71fe1c4210449844180e5a97cc295edd8ff
JuusoAsd <jahlroos96@gmail.com>
2024-04-20 22:07:04 +0300

    hc client

--- src/client.rs
+++ src/client.rs
@@ -2,13 +2,14 @@ use std::env;
 use std::time::Duration;
 
 use boolinator::Boolinator;
+use eyre::Context;
 use hex::encode as hex_encode;
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue, CONTENT_TYPE, USER_AGENT};
-use reqwest::Response;
 use reqwest::StatusCode;
+use reqwest::{Method, Response};
 use ring::hmac;
-use serde::de;
 use serde::de::DeserializeOwned;
+use serde::{de, Serialize};
 
 use crate::errors::error_messages;
 use crate::errors::*;
@@ -445,6 +446,98 @@ impl GenericClient {
         .collect();
 
         Ok(header)
+    }
+
+    // async fn send_request<S, T>(
+    //     &self,
+    //     host: &str,
+    //     endpoint: &str,
+    //     method: Method,
+    //     payload: S,
+    //     recv_window: u64,
+    // ) -> Result<T>
+    // where
+    //     S: Serialize,
+    //     T: DeserializeOwned,
+    // {
+    //     let request = match build_signed_request_p(payload, recv_window) {
+    //         Ok(r) => r,
+    //         Err(e) => {
+    //             return Err(Error::Msg(format!("Failed to build signed request: {}", e)));
+    //         }
+    //     };
+    //     let url = format!("{}{}", host, endpoint);
+    //     let response = match self
+    //         .inner
+    //         .request(method, &url)
+    //         .headers(self.build_headers(true)?)
+    //         .body(request)
+    //         .send()
+    //         .await
+    //     {
+    //         Ok(r) => r,
+    //         Err(e) => {
+    //             return Err(Error::Msg(format!("Failed to send request: {}", e)));
+    //         }
+    //     };
+
+    //     if let Err(e) = self.handler_check(&response).await {
+    //         return Err(Error::Msg(format!("Bad response: {}", e)));
+    //     }
+
+    //     let txt = response.text().await?;
+    //     let parsed = match serde_json::from_str(&txt) {
+    //         Ok(p) => p,
+    //         Err(e) => {
+    //             return Err(Error::Msg(format!("Failed to parse response: {} {}", txt, e)));
+    //         }
+    //     };
+    //     Ok(parsed)
+    // }
+
+    async fn send_request<S, T>(
+        &self,
+        host: &str,
+        endpoint: &str,
+        method: Method,
+        payload: S,
+        recv_window: u64,
+    ) -> eyre::Result<T>
+    where
+        S: Serialize,
+        T: DeserializeOwned,
+    {
+        let request = build_signed_request_p(payload, recv_window).wrap_err("Failed to build signed request")?;
+        let url = format!("{}{}", host, endpoint);
+        let response = self
+            .inner
+            .request(method, &url)
+            .headers(self.build_headers(true)?)
+            .body(request)
+            .send()
+            .await
+            .wrap_err("Failed to send request")?;
+
+        self.handler_check(&response)
+            .await
+            .wrap_err("Request handling failed")?;
+
+        let txt = response.text().await.wrap_err("Failed to read response text")?;
+        serde_json::from_str(&txt).wrap_err("Failed to parse response text into target type")
+    }
+
+    async fn handler_check(&self, response: &Response) -> Result<()> {
+        match response.status() {
+            StatusCode::OK => Ok(()),
+            StatusCode::INTERNAL_SERVER_ERROR => Err(Error::InternalServerError),
+            StatusCode::SERVICE_UNAVAILABLE => Err(Error::ServiceUnavailable),
+            StatusCode::UNAUTHORIZED => Err(Error::Unauthorized),
+            // StatusCode::BAD_REQUEST => {
+            //     let error: BinanceContentError = response.json().await?;
+            //     Err(handle_content_error(error))
+            // }
+            s => Err(Error::Msg(format!("Received response: {s:?}"))),
+        }
     }
 
     async fn handler<T: de::DeserializeOwned>(&self, response: Response) -> Result<T> {
